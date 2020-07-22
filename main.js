7b1ccf791bc8f0bff5353c4427e7a00aaea2d0e4
Vincent <23vincent86@gmail.com>
2020-07-22 15:50:16 +0200

    end

---
+++ main.js
@@ -0,0 +1,8 @@
+//mostra menu
+$(".header-right>a").click(function() {
+    $(".hamburger-menu").show("fast");
+});
+//chiudi menu
+$(".close").click(function() {
+    $(".hamburger-menu").hide("fast");
+});
